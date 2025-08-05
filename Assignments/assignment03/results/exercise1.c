3a058700b2842c68a41a5b4176c59efa0cb90970
eduabdala <eduardoabdala@icloud.com>
2025-08-04 21:47:39 -0300

    feat: exercise1 result

--- Assignments/assignment03/results/exercise1.c
+++ Assignments/assignment03/results/exercise1.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 
-// TO-DO: Use pointers and addresses to modify the code
-
-void square(int num)
+void square(int* num)
 {
-    num = num * num;
+    *num = (*num) * (*num);
 }
 
 int main()
 {
     int x = 4;
-    square(x);
-    printf("%d\n", x);
+    int *num = &x;
+    square(num);
+    printf("%d\n", *num);
     return 0;
 }
